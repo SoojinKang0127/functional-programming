ab5391517c3c93cc8019927adc868e180669ae0d
SoojinKang0127 <iamhajaml@naver.com>
2022-05-09 22:36:23 +0900

    Add reduce, go fx

--- intro/function.js
+++ intro/function.js
@@ -1,19 +1,39 @@
 const log = console.log;
 
 const map = (fn, iter) => {
-    const result = [];
-    for(const e of iter){
-        result.push(fn(e))
-    }
-    return result;
-}
+  const result = [];
+  for (const e of iter) {
+    result.push(fn(e));
+  }
+  return result;
+};
 
 const filter = (fn, iter) => {
-    const result = [];
-    for(const e of iter){
-        if(fn(e)){
-            result.push(e)
-        }
+  const result = [];
+  for (const e of iter) {
+    if (fn(e)) {
+      result.push(e);
     }
-    return result;
-}
+  }
+  return result;
+};
+
+const _reduce = (fn, acc, iter) => {
+  for (const e of iter) {
+    acc = fn(acc, e);
+  }
+  return acc;
+};
+
+const reduce = (fn, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const e of iter) {
+    acc = fn(acc, e);
+  }
+  return acc;
+};
+
+const go = (...args) => reduce((a, fn) => fn(a), args);
